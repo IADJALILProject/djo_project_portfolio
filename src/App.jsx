ed51272b89d0d706dc2b19bdbdf030a68edb24c1
Abdeldjalil Salah-Bey <abdeldjalil.salah-bey@powerup-technology.com>
2025-09-25 17:16:09 +0200

    okde

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const DATA = {
   phone: "tel:+33611279153",
   github: "https://github.com/IADJALILProject",
   linkedin: "https://www.linkedin.com/in/djalil-salah-bey/",
-  cvUrl: "/CV_DAbdeldjalil.pdf",
+  cvUrl: "/Malt_CDI_Djalil.pdf",
   avatar: "/avatar.jpg",
 };
 
