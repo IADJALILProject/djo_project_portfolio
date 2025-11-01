21082504ad87ad227838f5674907f2d6bbe540a2
Djalil_Salah-Bey <salahbeydjalil@gmail.com>
2025-11-01 18:51:49 +0100

    de

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const DATA = {
   phone: "tel:+33611279153",
   github: "https://github.com/IADJALILProject",
   linkedin: "https://www.linkedin.com/in/djalil-salah-bey/",
-  cvUrl: "/Djalil__Data_Engineer.pdf",
+  cvUrl: "/DjalilCV.pdf",
   avatar: "/avatar.jpg",
 };
 
